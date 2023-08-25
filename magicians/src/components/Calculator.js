3cba71e2c12158407a9c0ebcd2e5369aa648c91f
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-25 20:30:32 +0300

    Set up react
    I set up react Application

---
+++ magicians/src/components/Calculator.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function calculator_function(){
+
+   return(
+     <div className="maindiv">
+        <div> </div>
+        <div> <div>1</div><div>2</div><div>3</div><div>4</div> </div>
+        <div> <div>1</div><div>2</div><div>3</div><div>4</div> </div>
+        <div> <div>1</div><div>2</div><div>3</div><div>4</div> </div>
+        <div> <div>1</div><div>2</div><div>3</div><div>4</div> </div>
+        <div> <div>3</div><div>4</div> </div>
+
+    </div>
+    )
+}
+export default calculator_function;
